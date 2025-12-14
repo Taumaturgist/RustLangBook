ad23fc7c0cbf9239530cef7bd9fa687267d08896
Thaumaturgist <va.alekseevskiy@gmail.com>
2025-12-14 09:20:24 +0700

    move project

--- Chapter03/fibonacci/src/main.rs
+++ Chapter03/fibonacci/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("Hello! This is a Fibonacci generator!");
 
     loop {
-        println!("Enter the number of Fibonacci sequence member (i32 max) to calculate it!");
+        println!("Enter the number of Fibonacci sequence member (i128 max) to calculate it!");
 
         let mut fib_number = String::new();
         
@@ -12,7 +12,7 @@ fn main() {
             .read_line(&mut fib_number)
             .expect("Failed to read line");
 
-        let fib_number: i32 = match fib_number.trim().parse() {
+        let fib_number: i128 = match fib_number.trim().parse() {
             Ok(num) => num, 
             Err(_) => {
                 println!("Error: only digits input allowed");
@@ -26,24 +26,7 @@ fn main() {
     }    
 }
 
-fn calculate_fibonacci_pos (num: i32) -> i32 {
+fn calculate_fibonacci_pos (num: i128) -> i128 {
 
-    let mut a = vec![0; num as usize];    
-
-    let mut index = 0;
-
-    for element in a {
-        if index == 0 {
-            element = 0;
-            index += 1;
-        } else if index == 1 {
-            element = 1;
-            index += 1;
-        } else {
-            element = a[index - 2] + a[index - 1];
-            index += 1;
-        }
-    }    
-
-    a[num as usize - 1]
+   
 }
